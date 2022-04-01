10ec5ef82667ace0940dbce60d86d4463ab41a67
d5747 <d5747@LAPTOP-23PGKB21>
2022-04-01 15:01:27 +0800

    加入專案檔案。

---
+++ Json2CSharp/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Json2CSharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.JsonText = new System.Windows.Forms.TextBox();
+            this.ClassText = new System.Windows.Forms.TextBox();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // JsonText
+            // 
+            this.JsonText.Location = new System.Drawing.Point(27, 30);
+            this.JsonText.Multiline = true;
+            this.JsonText.Name = "JsonText";
+            this.JsonText.Size = new System.Drawing.Size(286, 408);
+            this.JsonText.TabIndex = 0;
+            // 
+            // ClassText
+            // 
+            this.ClassText.Location = new System.Drawing.Point(451, 30);
+            this.ClassText.Multiline = true;
+            this.ClassText.Name = "ClassText";
+            this.ClassText.Size = new System.Drawing.Size(277, 408);
+            this.ClassText.TabIndex = 1;
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(344, 172);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(75, 23);
+            this.btnConvert.TabIndex = 2;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.ClassText);
+            this.Controls.Add(this.JsonText);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox JsonText;
+        private System.Windows.Forms.TextBox ClassText;
+        private System.Windows.Forms.Button btnConvert;
+    }
+}
+
---
+++ Json2CSharp/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Json2CSharp
+{
+    public partial class Form1 : Form
+    {
+        private List<string> classes = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ClassText.Text = string.Empty;
+            var obj = JsonToObject(JsonText.Text);
+            classes.Add(ObjectToClass(obj));
+            var context = string.Empty;
+            classes.ForEach(o => context = context + o + "\r\n");
+            ClassText.Text = context;
+        }
+
+        private JObject JsonToObject(string str)
+        {
+            var obj = default(JObject);
+
+            if (string.IsNullOrEmpty(str))
+            {
+                MessageBox.Show("請輸入JSON");
+                return obj;
+            }
+            if (str.Trim().First() != '{' || str.Trim().Last() != '}')
+            {
+                MessageBox.Show("請輸入JSON");
+                return obj;
+            }
+            try
+            {
+                return JsonConvert.DeserializeObject<JObject>(str.Replace("\r","").Replace("\n", "").Trim());
+            }
+            catch (JsonException ex)
+            {
+                MessageBox.Show(ex.ToString());
+                return obj;
+            }
+        }
+
+
+        private string ObjectToClass(JObject jobj)
+        {
+            if (jobj == null) return string.Empty;
+            if (jobj.Children().Count() <= 0) return string.Empty;
+
+            var contact = string.Empty;
+            foreach (var child in jobj.Children())
+            {
+                contact += (child.HasValues) ? GetPropretityStr(child) : $" public string {child.Path} {{ get; set; }}\r\n";
+            }
+
+            var classStr = $" class NewClass{classes.Count +1 } {{\r\n " + contact + "\r\n}";
+
+
+            return classStr;
+        }
+
+
+        private string GetPropretityStr(JToken child)
+        {
+            var val = child.ToString().Split(':').Skip(1);
+
+            return $" public {GetTypeStr(string.Join(":",val))} {child.Path} {{ get; set; }}\r\n";
+        }
+
+        private string GetTypeStr(string val)
+        {
+            val = val.Replace("\r", "").Replace("\n", "").Trim();
+            var ptype = "string";
+            if (val.First() == '[')
+            {
+                var a = JsonConvert.DeserializeObject(val);
+                ptype = "IEnumerable<" + IEnumerableParse(JsonConvert.DeserializeObject<IEnumerable<object>>(val)) + ">";
+            }
+            else if (val.First() == '{') {
+                classes.Add(ObjectToClass(JsonConvert.DeserializeObject<JObject>(val)));
+                ptype = "NewClass" + classes.Count;
+            }
+            else if (val.First() == '"')
+            {
+                var d = DateTime.Now;
+                if (DateTime.TryParse(val, out d)) ptype = ptype = "DateTime?";
+            }
+            else
+            {
+                if (val.ToUpper() == "TRUE" || val.ToUpper() == "FALSE") ptype = "bool?";
+                else
+                {
+                    if (val.IndexOf('.') > -1)
+                    {
+                        var a = 0M;
+                        if (Decimal.TryParse(val, out a)) ptype = "Decimal?";
+                    }
+                    else
+                    {
+                        var a = 0;
+                        var _ = 0L;
+                        if (int.TryParse(val, out a)) ptype = "int?";
+                        else if (long.TryParse(val, out _)) ptype = "long?";
+                    }
+                }
+            }
+            return ptype;
+        }
+
+        private string IEnumerableParse(IEnumerable<object> objs)
+        {
+            var firstChars = objs.Select(o => o.ToString().First());
+            var allTheSame = firstChars.Any(c => c == '{')? firstChars.All(c => c == '{') : true;
+            allTheSame = allTheSame && firstChars.Any(c => c == '[') ? firstChars.All(c => c == '[') : true;
+            allTheSame = allTheSame && firstChars.Any(c => c == '"') ? firstChars.All(c => c == '"') : true;
+            if (!allTheSame)
+            {
+                return "object";
+            }
+            else
+            {
+                string ptype = null;
+                foreach (var obj in objs) {
+                    var tmp = GetTypeStr(obj.ToString());
+                    if (ptype == null) ptype = tmp;
+                    else {
+                        if (ptype != tmp) {
+                            if ((ptype == "long?" || ptype == "int?" || ptype == "Decimal?") && (tmp == "long?" || tmp == "int?" || tmp == "Decimal?"))
+                            {
+                                if (ptype == "Decimal?" || tmp == "Decimal?")
+                                {
+                                    ptype = "Decimal?";
+                                    continue;
+                                }
+                                if (ptype == "long?" || tmp == "long?")
+                                {
+                                    ptype = "long?";
+                                    continue;
+                                }
+                                ptype = "int?";
+                            }
+                            else
+                            {
+                                if ((ptype == "dateTime?" || ptype == "string") && (tmp == "dateTime?" || tmp == "string"))
+                                {
+                                    ptype = "string";
+                                }
+                                else
+                                {
+                                    ptype = "object";
+                                }
+                                break;
+                            }
+                        }
+                    }
+                }
+                return ptype;
+            }
+        }
+    }
+}
+
